322226ff4d43049f1d7ba6aed8e5e5b72bb82f9d
jinx71 <karim.sazed@gmail.com>
2021-10-16 00:41:54 +0000

    feat: setup authentication routes

---
+++ pitchside/server/src/routes/authRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { body } = require('express-validator');
+const { register, login, me, updateFavorites } = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.post(
+  '/register',
+  [
+    body('name').trim().notEmpty().withMessage('Name required'),
+    body('email').isEmail().withMessage('Valid email required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be 6+ chars'),
+  ],
+  register
+);
+
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Valid email required'),
+    body('password').notEmpty().withMessage('Password required'),
+  ],
+  login
+);
+
+router.get('/me', protect, me);
+router.put('/favorites', protect, updateFavorites);
+
+module.exports = router;
